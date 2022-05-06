6d51d04d181300b0f4fba9a8f72475902cc2135f
David <rector@rdacorp.com>
2022-05-06 08:36:52 -0400

    WIP

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className="d-flex justify-content-center mt-5">
-        <h1 className="display-1">Welcome to David's Grocery!</h1>
+        <h1 className="display-1">Welcome to David's Grocery! {process.env.NEXT_PUBLIC_TEST}</h1>
       </div>
     </>
   );
