28333ce476c2f845a2eb7993e89971dcf2ef71dd
David <rector@rdacorp.com>
2022-05-12 11:25:56 -0400

    signalr

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,19 @@ import Layout from '../components/layout';
 import { MsalProvider } from '@azure/msal-react';
 import { msalInstance } from '../services/msal';
 import { GroceryServiceProvider } from '../components/grocery-service-context';
+import SignalRListener from '../components/signal-r-listener';
 
 export default function App({ Component, pageProps }) {
   
     return (
       <MsalProvider instance={msalInstance}>
-        <GroceryServiceProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </GroceryServiceProvider>
+        <SignalRListener>
+          <GroceryServiceProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </GroceryServiceProvider>
+        </SignalRListener>
       </MsalProvider>
     );
   }
